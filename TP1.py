04074880b00ad9c9d9120026d5442982611e549a
Luis Celestre <105753178+Lumacel@users.noreply.github.com>
2022-07-18 07:55:41 -0300

    Update TP1.py

--- TP1.py
+++ TP1.py
@@ -1,3 +1,5 @@
+##### TP1 etapa 3 ######
+
 import sys
 from os import system
 arch = ""
@@ -18,32 +20,29 @@ def ingresar_texto():  # Guarda texto ingresado en una variable
         arch += frase + "\n "
     return arch
 
-
-def levantar_texto(): # extrae texto de un archivo
+def levantar_texto(nombre_arch): # extrae texto de un archivo
     system('cls')
-    opciones= ('1','2','')
+    print('MODO ARCHIVO\n')
     arch = ''
-    while True:
-        system('cls')
-        nombre = input("MODO ARCHIVO\n\nIngrese el nombre del archivo: ")
+    opciones= ('1','2','')
+    while True:  
         try:
-            with open(nombre, 'r', encoding='utf-8') as texto:
+            with open(nombre_arch, 'r', encoding='utf-8') as texto:
                 for linea in texto:
                     linea = linea.lower()
                     arch += linea
             break
         except FileNotFoundError:
-            print('\nEl archivo no existe!')
-            opc = input('\n1- INTENTAR OTRA VEZ\t2- SALIR\n\nSu opción: ')
+            print('El archivo no existe!')
+            opc = input('\n1- INTENTAR CON OTRO NOMBRE DE ARCHIVO\n\n2- SALIR\n\nSu opción: ')
             while opc not in opciones:
-                opc = input('1- INTENTAR OTRA VEZ\t2- SALIR\n\nSu opción: ')
-            if opc == '1': continue
+                opc = input('1- INTENTAR CON OTRO NOMBRE DE ARCHIVO\n\n2- SALIR\n\nSu opción: ')
+            if opc == '1': 
+                system('cls')
+                nombre_arch = input("MODO ARCHIVO\n\nIngrese el nombre del archivo: ")
             else: break
-
     return arch
     
-      
-
 def eliminar_tildes(archivo): # Elimina tildes y signos del texto ingresado
     a,b,c = 'áéíóúü','aeiouu',"!#$¡%&'()*+,─—-./:;<=>?@[]^_`{|}~'"
     arch_2 = archivo.translate(archivo.maketrans(a,b,c))
@@ -64,8 +63,7 @@ def buscar_faltantes(archivo): # Busca palabras faltantes en el diccionario y cr
                         break
             if not coincidencia:
                 if palabra not in inexistentes:
-                    inexistentes.append(palabra)  
-                
+                    inexistentes.append(palabra)       
     return inexistentes 
 
 def mostrar_resultado(inexistentes):
@@ -73,30 +71,28 @@ def mostrar_resultado(inexistentes):
     if len(inexistentes) == 0: print("\nTodas las palabras están en el diccionario.")
     else : print(f"\nEl texto contiene {c} {'palabras' if c > 1 else 'palabra'} que no {'están' if c> 1 else 'está'} en el diccionario\n")        
     
-    for i in inexistentes:
-        print(i)   
-    input("")              
-############## PROGRAMA PRINCIPAL ############
 
-while True:
-    system('cls')
-    opciones = ('1','2')
-    opcion= input('''CORRECTOR DE TEXTO:\n
-1- MODO INTERACTIVO\n 
-2- MODO ARCHIVO\n
-Elija una opción: ''')
+def agregar_diccionario():
+    #with open('spanish.lst', 'a' )
+    pass
 
-    if opcion not in opciones:
-        continue
-    elif opcion == '1':
-        arch = ingresar_texto()
-        arch_2 = eliminar_tildes(arch)
-        inexistentes = buscar_faltantes(arch_2)
-        mostrar_resultado(inexistentes)
+def modificar_palabra():
+    pass     
 
-    else:
-        arch = levantar_texto()
-        if arch == '': continue
+
+############## PROGRAMA PRINCIPAL ####################
+ 
+n = len(sys.argv)# cantidad de argumentos pasada desde consola
+
+if n == 1:
+    arch = ingresar_texto()
+    arch_2 = eliminar_tildes(arch)
+    inexistentes = buscar_faltantes(arch_2)
+    mostrar_resultado(inexistentes)
+
+else:
+    arch = levantar_texto(sys.argv[1])
+    if arch != '': 
         arch_2 = (eliminar_tildes(arch))
         inexistentes = buscar_faltantes(arch_2)
         mostrar_resultado(inexistentes)
